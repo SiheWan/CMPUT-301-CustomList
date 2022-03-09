db82ae86d3cbdf315643dce5e3fac8207df62f68
Sihe Wan <sihe@ualberta.ca>
2022-03-09 01:11:17 -0700

    test case for hasCity

--- app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
+++ app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
@@ -61,7 +61,11 @@ public class CustomList extends ArrayAdapter<City> {
      */
 
     public boolean hasCity(City city){
-        return false;
+        if (cities.contains(city)){
+            return true;
+        }
+        else
+            return false;
     }
 
 
